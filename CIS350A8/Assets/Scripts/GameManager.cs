a562dbb1ef3788f196b297d2ff3740596b3d60e0
sigavg <lesimplu@gmail.com>
2022-04-23 13:13:42 -0500

    1

---
+++ CIS350A8/Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+﻿/* Luke Lesimple
+ * Prototype 5
+ * Game manager
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> objs;
+
+    private float SpawnRate = 1.0f;
+
+    public TextMeshProUGUI scoreText;
+    private int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
+    }
+
+    public void UpdateScore(int toAdd)
+    {
+        score += toAdd;
+        scoreText.text = "Score:\n" + score;
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(SpawnRate);
+            int index = Random.Range(0, objs.Count);
+            Instantiate(objs[index]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ CIS350A8/Assets/Scripts/Target.cs
@@ -0,0 +1,71 @@
+﻿/* Luke Lesimple
+ * Prototype 5
+ * Spawns Objects
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody targetRB;
+    private float minSpeed = 12;
+    private float maxSpeed = 16;
+    private float maxTorque = 10;
+    private float xRange = 4;
+    private float ySpawnPos = -6;
+
+    private GameManager gameMan;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRB = GetComponent<Rigidbody>();
+
+        targetRB.AddForce(RandomForce(), ForceMode.Impulse);
+
+        targetRB.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+
+        transform.position = RandomSpawnPos();
+
+        gameMan = GameObject.FindGameObjectWithTag("GM").GetComponent<GameManager>();
+
+    }
+
+    private Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
+    }
+
+    private float RandomTorque()
+    {
+        return Random.Range(- maxTorque, maxTorque);
+    }
+
+    private Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        gameMan.UpdateScore(1);
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+
+
+}
